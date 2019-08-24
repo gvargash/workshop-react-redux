f21656c069c1b92e49685e9eb30f44aed66c4f13
gvargash <gvargash2@gmail.com>
2019-08-23 20:49:18 -0500

    sobre escribiendo cra

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Button} from "antd"
 const App = () => {
   return (
     <div>
+        <Button type="primary">text</Button>
         Plataforma de Educación Online
     </div>
   )
