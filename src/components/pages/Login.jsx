242698281c1f36cc0fe3a2bf51aa69a1a56679a5
gvargash <gvargash2@gmail.com>
2019-09-03 23:24:08 -0500

    registro y login de usuario

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,50 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Icon, Input, Button, Checkbox, Col, Row } from "antd";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import Axios from "axios";
 
 const Login = props => {
   const { getFieldDecorator, validateFields } = props.form;
-
+  const [loginState, setloginState] = useState(false);
   const handleSubmit = e => {
     e.preventDefault();
-    validateFields((err, values) => {
-      if (!err) {
-        console.log("Received values of form: ", values);
+    validateFields(async (err, values) => {
+      try {
+        if (!err) {
+          const data = { email: values.email, password: values.password };
+          const response = await Axios.post(`${process.env.REACT_APP_API_USER}/login`, data);
+          if (response.data.token) {
+            localStorage.setItem("token", response.data.token);
+            setloginState(true);
+          }
+        }
+      } catch (error) {
+        console.log("error:", error.message);
       }
     });
   };
-  
+
   return (
     <>
-      <Row type="flex" justify="center">
+      <Row type='flex' justify='center'>
         <Col>
-          <Form onSubmit={handleSubmit} className="login-form" id="components-login-form-q">
-            <div className="header-q">
+          <Form onSubmit={handleSubmit} className='login-form' id='components-login-form-q'>
+            <div className='header-q'>
               <h1>Iniciar sesión</h1>
             </div>
+
             <Form.Item>
-              {getFieldDecorator("username", {
-                rules: [{ required: true, message: "Por favor Ingresa tu username!" }]
-              })(<Input prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />} placeholder="Username" />)}
+              {getFieldDecorator("email", {
+                rules: [{ required: true, message: "Por favor Ingresa tu correo!" }]
+              })(<Input prefix={<Icon type='mail' style={{ color: "rgba(0,0,0,.25)" }} />} type='email' placeholder='correo@gmail.com' />)}
             </Form.Item>
+
             <Form.Item>
               {getFieldDecorator("password", {
                 rules: [{ required: true, message: "Por favor Ingresa tu Password!" }]
-              })(<Input prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />} type="password" placeholder="Password" />)}
+              })(<Input prefix={<Icon type='lock' style={{ color: "rgba(0,0,0,.25)" }} />} type='password' placeholder='Password' />)}
             </Form.Item>
+
             <Form.Item>
               {getFieldDecorator("remember", {
                 valuePropName: "checked",
                 initialValue: true
               })(<Checkbox>Recuerdame</Checkbox>)}
-              <Link className="login-form-forgot" to="">
+              <Link className='login-form-forgot' to=''>
                 Olvide mi contraseña
               </Link>
-              <Button type="primary" htmlType="submit" className="login-form-button">
+              <Button type='primary' htmlType='submit' className='login-form-button'>
                 Iniciar sesión
               </Button>
-              O <Link to="/register">Registrate ahora!</Link>
+              O <Link to='/register'>Registrate ahora!</Link>
             </Form.Item>
+            {loginState && <Redirect to='/'></Redirect>}
           </Form>
         </Col>
       </Row>
--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,9 +1,69 @@
 import React from "react";
+import { Form, Icon, Input, Button, Col, Row } from "antd";
+import { Link, Redirect } from "react-router-dom";
+import Axios from "axios";
 
-const Register = () => (
-  <div>
-    <h1>Register</h1>
-  </div>
-);
+const Register = props => {
+  const { getFieldDecorator, validateFields } = props.form;
+  const handleSubmit = e => {
+    e.preventDefault();
+    validateFields(async (err, values) => {
+      try {
+        if (!err) {
+          const data = {
+            email: values.email,
+            name: values.name,
+            password: values.password
+          };
+          const resp = await Axios.post(`${process.env.REACT_APP_API_USER}/signup`, data);
+          if (resp.data) {
+            return <Redirect to='/login' />;
+          }
+        }
+      } catch (error) {
+        console.log("error:", error.message);
+      }
+    });
+  };
 
-export default Register;
+  return (
+    <>
+      <Row type='flex' justify='center'>
+        <Col span={6}>
+          <Form onSubmit={handleSubmit} className='register-form' id='components-register-form-q'>
+            <div className='header-q'>
+              <h1>Crear cuenta</h1>
+            </div>
+
+            <Form.Item>
+              {getFieldDecorator("name", {
+                rules: [{ required: true, message: "Por favor Ingresa tu nombre!" }]
+              })(<Input prefix={<Icon type='user' style={{ color: "rgba(0,0,0,.25)" }} />} placeholder='Raul Romeor' />)}
+            </Form.Item>
+
+            <Form.Item>
+              {getFieldDecorator("email", {
+                rules: [{ required: true, message: "Por favor Ingresa tu correo!" }]
+              })(<Input prefix={<Icon type='mail' style={{ color: "rgba(0,0,0,.25)" }} />} type='email' placeholder='correo@gmail.com' />)}
+            </Form.Item>
+
+            <Form.Item>
+              {getFieldDecorator("password", {
+                rules: [{ required: true, message: "Por favor Ingresa tu Password!" }]
+              })(<Input prefix={<Icon type='lock' style={{ color: "rgba(0,0,0,.25)" }} />} type='password' placeholder='Password' />)}
+            </Form.Item>
+
+            <Form.Item>
+              <Button type='primary' htmlType='submit' block className='login-form-button'>
+                Registrar cuenta
+              </Button>
+              O <Link to='/login'>Iniciar sesión!</Link>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
+};
+const FormRegister = Form.create({ name: "register_form" })(Register);
+export default FormRegister;
