703feb6be8033b9fcbb09759e1e8325ca14ef1ca
gvargash <gvargash2@gmail.com>
2019-09-03 23:26:09 -0500

    Configura rutas publicas y privadas

--- src/components/Routes/Public.jsx
+++ src/components/Routes/Public.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import {Redirect,Route} from 'react-router-dom'
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
 
-const Public = ({component:Component,...rest}) => {
-  const userLogged=false
-  if(userLogged){
-    return <Redirect to="/"/>
+const Public = ({ component: Component, ...rest }) => {
+  const userLogged = localStorage.getItem("token");
+  if (userLogged) {
+    return <Redirect to='/' />;
   }
-  return <Route {...rest} component={Component}/>
-}
+  return <Route {...rest} component={Component} />;
+};
 
-export default Public
+export default Public;
