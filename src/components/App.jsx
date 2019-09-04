a820df0c83b32b0871168f656c79a44569bdb2a1
gvargash <gvargash2@gmail.com>
2019-09-03 23:26:48 -0500

    Renombrando nombre de componentes de login y register

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import Courses from "./pages/Courses";
 import Course from "./pages/Course";
 import Teachers from "./pages/Teachers";
 import Fragment from "./pages/Fragment";
-import Register from "./pages/Register";
+import FormRegister from "./pages/Register";
 import Page404 from "./pages/Page404";
 import WrappedLoginForm from "./pages/Login";
 import HeaderQ from "./organisms/HeaderQ";
@@ -19,21 +19,21 @@ const { Content, Footer } = Layout;
 const App = () => {
   return (
     <Router>
-      <Layout className="layout" id="layout-template-q">
+      <Layout className='layout' id='layout-template-q'>
         <HeaderQ />
-        <Content className="main-container-q">
+        <Content className='main-container-q'>
           <div style={{ padding: 24, minHeight: "100vh" }}>
             <Switch>
-              <Protected path="/" exact component={Home} />
-              <Protected path="/especialidades" exact component={Specialities} />
-              <Protected path="/especialidades/:id" component={Speciality} />
-              <Protected path="/cursos" exact component={Courses} />
-              <Protected path="/cursos/:id" component={Course} />
-              <Protected path="/profesores" exact component={Teachers} />
-              <Protected path="/clase/:id" component={Fragment} />
+              <Protected path='/' exact component={Home} />
+              <Protected path='/especialidades' exact component={Specialities} />
+              <Protected path='/especialidades/:id' component={Speciality} />
+              <Protected path='/cursos' exact component={Courses} />
+              <Protected path='/cursos/:id' component={Course} />
+              <Protected path='/profesores' exact component={Teachers} />
+              <Protected path='/clase/:id' component={Fragment} />
 
-              <Public path="/login" exact component={WrappedLoginForm} />
-              <Public path="/register" exact component={Register} />
+              <Public path='/login' exact component={WrappedLoginForm} />
+              <Public path='/register' exact component={FormRegister} />
 
               <Route component={Page404} />
             </Switch>
